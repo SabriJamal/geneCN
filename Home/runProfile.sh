b8b6289f0d0d90ffea67016eccdffc53571d4074
Sabri Jamal <sjamal@login1.prv.davros.compute.estate>
2018-10-02 10:24:58 +0100

    Comitting changes made to runProfile.sh script

--- Home/runProfile.sh
+++ Home/runProfile.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #BSUB -P DMPMXHAAC
-#BSUB -J CrNPPAED
+#BSUB -J CrNPGSIC
 #BSUB -n 2
-#BSUB -e CrNPPAED.e
-#BSUB -o CrNPPAED.o
+#BSUB -e CrNPGSIC.e
+#BSUB -o CrNPGSIC.o
 
 #sh 2.createNP_profile.sh ABCBIO normals
 #sh 2.createNP_profile.sh RMH200 tumours
-sh 2.createNP_profile.sh PAED normals
+#sh 2.createNP_profile.sh PAED normals
+sh 2.createNP_profile.sh GSIC normals
