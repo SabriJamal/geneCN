0b09d5ba44d4d8f09b115cc9dc4bb49b4b72792c
Sabri Jamal <s.jamal@live.se>
2018-10-19 10:50:52 +0100

    Added ploidy calculation to script

--- CNVkit_tests/Result/PooledReference/CNV_kit_ABCBIO_9600/AdditionalTests/get_CNA.py
+++ CNVkit_tests/Result/PooledReference/CNV_kit_ABCBIO_9600/AdditionalTests/get_CNA.py
@@ -2,10 +2,13 @@
 
 import os
 import re
-import pdb
-pdb.set_trace()
+#import pdb #debugging
+#pdb.set_trace() #debugging
 
-def get_CNA(CNA_in, CNA_out, bed, thresh=0.2):
+###################################################################################
+## Calculates and outputs ROIs above and below thresh hold as tab delimited file ##
+###################################################################################
+def get_CNA(CNA_in, CNA_out, bed, thresh=0.2, ploidy=2):
 	file_in = CNA_in
 	file_out = CNA_out
 	CNA_dict = {}
@@ -30,6 +33,7 @@ def get_CNA(CNA_in, CNA_out, bed, thresh=0.2):
 				if(control):
 					match[0] = control.group(1) + control.group(2) + control.group(3) + "_" + control.group(4)
 
+			####################################
 			## Store CNA stat data per target ##
 			####################################
 			try:
@@ -42,7 +46,7 @@ def get_CNA(CNA_in, CNA_out, bed, thresh=0.2):
 			except IndexError:
 				print("Index error was caught, does report contain empty lines at EOF?")
 
-
+			######################################################################
 			## Collect CNA data for each target called 							##
 			## stores in array in form [ [exon1, exon2], [ [stat1], [stat2] ] ] ##
 			######################################################################
@@ -56,7 +60,7 @@ def get_CNA(CNA_in, CNA_out, bed, thresh=0.2):
 					data_tuple[1].append([log2,depth,weight,nr_bins,segment_weight, segment_probes])
 					CNA_dict[gene[0]] = data_tuple
 
-
+############################
 ## Output CNA report file ##
 ############################
 	with open(file_out, "w") as OUT:
@@ -70,6 +74,8 @@ def get_CNA(CNA_in, CNA_out, bed, thresh=0.2):
 			amp_exon_perc = 0
 			min_log2 = 0
 			max_log2 = 0
+			min_copies = 0
+			max_copies = 0
 
 			nr_ROI = ROI_tot_count_dict[gene_name]
 			new_gene_min = 1
@@ -77,52 +83,72 @@ def get_CNA(CNA_in, CNA_out, bed, thresh=0.2):
 			## Loop through each exon (to extract stats)
 			for i in range(0, len(val_array[0])):
 				exon_name = val_array[0][i]
+				log2_value = float(val_array[1][i][0])
 
-				## Loop through each stat (for each exon)
-				for k in range(0, len(val_array[1][i])):
-					log2_value = float(val_array[1][i][0])
-
-					#Set initial min value
-					if(new_gene_min == 1):
-						min_log2 = log2_value
-						new_gene_min = 0
-
-					#Set initial max value
-					if(new_gene_max == 1):
-						max_log2 = log2_value
-						new_gene_max = 0
-
-					#Replace min value if higher
-					if(log2_value < min_log2):
-						min_log2 = log2_value
-
-					#Replace max value if higher
-					if(log2_value > max_log2):
-						max_log2 = log2_value
-
-					#Find exons called as deletions
-					if(log2_value < -thresh): # log2 value
-						del_exon_counter += 1
+				#Find exons called as deletions
+				if(log2_value < -thresh): # log2 value
+					del_exon_counter += 1
+					if(del_exon_names == ""):
+						del_exon_names = exon_name
+					else:
 						del_exon_names = del_exon_names + "," + exon_name
 
-					#Find exons called as amplifications
-					if(log2_value > thresh):
-						amp_exon_counter += 1
+				#Find exons called as amplifications
+				if(log2_value > thresh):
+					amp_exon_counter += 1
+					if(amp_exon_names == ""):
+						amp_exon_names = exon_name
+					else:
 						amp_exon_names = amp_exon_names + "," + exon_name
 
-				##Calc percentage exons called as deletions & amplification
-				del_exon_perc = (del_exon_counter/nr_ROI)*100
-				amp_exon_perc = (amp_exon_counter/nr_ROI)*100
-				OUT.write(gene_name + "\t" + str(min_log2) + "\t" + str(max_log2) + "\t" + str(nr_ROI) + "\t" + str(del_exon_counter) + "\t" + del_exon_names + "\t" + str(amp_exon_counter) + "\t" + amp_exon_names + "\t" + str(del_exon_perc) + "\t" + str(amp_exon_perc) + "\n")
+				#Set initial min value
+				if(new_gene_min == 1):
+					min_log2 = log2_value
+					new_gene_min = 0
 
-		#for item in val[0]:
-		#	try:
-		#		if(int(item)):
+				#Set initial max value
+				if(new_gene_max == 1):
+					max_log2 = log2_value
+					new_gene_max = 0
 
-		#			print("type conversion worked for: " + str(item))
-		#	except:
-		#		print("type conversion failed for: " + item)
+				#Replace min value if higher
+				if(log2_value < min_log2):
+					min_log2 = log2_value
 
+				#Replace max value if higher
+				if(log2_value > max_log2):
+					max_log2 = log2_value
+
+				##################################################################
+				## Loop through each stat (for each exon)						##
+				## Note! Disabled funcitonality, (not outputted to report)		##
+				## Reason is that currently it is assumed that every exon		##
+				## in a gene will have the same stat i.e. Come from the same	##
+				## segment														##
+				##################################################################
+				for k in range(1, len(val_array[1][i])):
+					if(k == 1):
+						data_as_text = val_array[1][i][k]
+					elif(k <= len(val_array[1][i])):
+						data_as_text = data_as_text + "\t" + val_array[1][i][k]
+					#elif(k == len(val_array[1][i])):
+					#	data_as_text = data_as_text + "\t" + val_array[1][i][k]
+
+
+			##Calc percentage exons called as deletions & amplification
+			del_exon_perc = (del_exon_counter/nr_ROI)*100
+			amp_exon_perc = (amp_exon_counter/nr_ROI)*100
+			min_copies = ploidy*2**(min_log2)
+			max_copies = ploidy*2**(max_log2)
+
+			OUT.write(gene_name + "\t" + str(min_log2) + "\t" + str(max_log2) + "\t" + str(nr_ROI) + "\t" + str(del_exon_counter) + "\t" + del_exon_names + "\t" + str(amp_exon_counter) + "\t" + amp_exon_names + "\t" + str(del_exon_perc) + "\t" + str(amp_exon_perc) + "\t" + str(min_copies) + "\t" + str(max_copies) + "\n")
+
+			#Below writes segmentation data to file as well. Although assumes that a gene cannot exist in two segments
+			#OUT.write(gene_name + "\t" + str(min_log2) + "\t" + str(max_log2) + "\t" + str(nr_ROI) + "\t" + str(del_exon_counter) + "\t" + del_exon_names + "\t" + str(amp_exon_counter) + "\t" + amp_exon_names + "\t" + str(del_exon_perc) + "\t" + str(amp_exon_perc) + "\t" + data_as_text + "\n")
+
+###############################################################
+## Reads in bed file to extract number of ROIs for each gene ##
+###############################################################
 def Extract_ROI_per_gene(bed):
 	file_in = bed
 	ROI_dict = {}
@@ -149,6 +175,7 @@ def Extract_ROI_per_gene(bed):
 					add_exon.append(roi[1])
 					ROI_dict[roi[0]] = add_exon
 
+	###################################################
 	## Count number of ROIs per target from bed file ##
 	###################################################
 	ROI_tot_count_dict = {}
@@ -169,5 +196,4 @@ def Extract_ROI_per_gene(bed):
 
 
 
-#Extract_ROI_per_gene("/Users/sjamal/Documents/Work/2.scripts/geneCN/CNVkit_tests/Result/PooledReference/CNV_kit_ABCBIO_9600/ABCBIO.reference.target-tmp.bed")
 get_CNA("gene_CNA_events.txt", "test.txt", "/Users/sjamal/Documents/Work/2.scripts/geneCN/CNVkit_tests/Result/PooledReference/CNV_kit_ABCBIO_9600/ABCBIO.reference.target-tmp.bed")
